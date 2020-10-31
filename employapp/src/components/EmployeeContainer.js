acd9fd022ed80379c47aebc9a5bada66d430dd7b
Rayan Ahmedy <rayanahmedy1999@gmail.com>
2020-10-31 00:28:15 -0400

    file setup for react

---
+++ employapp/src/components/EmployeeContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Employee from './Employee';
+// import Search from "./Search";
+
+function EmployeeContainer(props) {
+	return;
+	<div className="container" style={props.style}>
+	</div>;
+}
+
+export default EmployeeContainer;
