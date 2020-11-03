f933bbf5f2fda0de5a1e5c7293c489f7e28b9985
Rayan Ahmedy <rayanahmedy1999@gmail.com>
2020-11-03 18:12:00 -0500

    Updates

---
+++ precache-manifest.7a8c3d3567012459c3960dad9140233b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30477ea53bf0fcf45351f28c4a9d52e0",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "b6ca03d0722a05710f1d",
+    "url": "/React-Employee-Directory/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "053f393cd2726875b922",
+    "url": "/React-Employee-Directory/static/css/main.3f42b2f1.chunk.css"
+  },
+  {
+    "revision": "b6ca03d0722a05710f1d",
+    "url": "/React-Employee-Directory/static/js/2.d35abed9.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/React-Employee-Directory/static/js/2.d35abed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "053f393cd2726875b922",
+    "url": "/React-Employee-Directory/static/js/main.d9bfaa57.chunk.js"
+  },
+  {
+    "revision": "291608b71087120f2b73",
+    "url": "/React-Employee-Directory/static/js/runtime-main.7dc0ece2.js"
+  }
+]);
