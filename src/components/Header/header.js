93576d06d3e1680dbf4cfe7792b34923be908d73
Rayan Ahmedy <rayanahmedy1999@gmail.com>
2020-11-03 17:50:06 -0500

    added some styling to the header and thee table

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -6,7 +6,7 @@ const Header = (props) => {
     return (
         <div className="header sticky-top bg-dark text-white">
             <h1>Employee Directory</h1>
-            <button onClick={() => props.toTop()} type="button" class="btn btn-outline-light">Back to Top</button>
+            <button onClick={() => props.toTop()} type="button" class=" butt btn btn-outline-light">Back to Top</button>
         </div>
     )
 }
